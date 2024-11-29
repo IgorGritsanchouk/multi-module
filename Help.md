fc13f0096c5310ab72a5097dc3d5470ed1cbfb96
igor <igor.alexienco@gmail.com>
2024-11-29 15:13:11 +0000

    Added few red files

--- Help.md
+++ Help.md
@@ -6,7 +6,7 @@ Compile and package jar scanner-0.0.1-SNAPSHOT.jar and aero-0.0.1-SNAPSHOT.jar
 AND build images as below. Make sure Docker desktop is running
 3) webstn> docker build -f Dockerfile -t igr025/webstn-17-img:v0.1 .
 4) scanner> docker build -f Dockerfile -t igr025/scanner-17-img:v0.1 .
-
+5) jdbc:h2:mem:h2_db       ----database connection
 Create queue on active MQ broker:  scanner.queue
 scanner will write messages to it.
 
--- webstn/src/main/java/com/mm/webstn/component/MessageConsumer.java
+++ webstn/src/main/java/com/mm/webstn/component/MessageConsumer.java
@@ -1,14 +1,21 @@
 package com.mm.webstn.component;
 
 import com.mm.shared.domain.Message;
+import com.mm.webstn.domain.HistoryMessage;
+import com.mm.webstn.services.MessageConsumerService;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 @Component
 public class MessageConsumer {
+
+    @Autowired
+    MessageConsumerService messageConsumerService;
 
     private final String QUEUE = "scanner.queue";
 
@@ -19,6 +26,13 @@ public class MessageConsumer {
     public void receiveMessage(Message message) {
        System.out.println("Received message: "+message.getText());
         receivedMessages.add(message.getText());
+
+        Date date = new Date(System.currentTimeMillis());
+        HistoryMessage historyMessage = new HistoryMessage();
+        historyMessage.setDate(date.toInstant());
+        historyMessage.setMessage(message.getText());
+        messageConsumerService.saveHistoryMessage(historyMessage);
+
     }
 
 }
