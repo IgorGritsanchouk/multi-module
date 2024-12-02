1772873c680168789affa7ff0837ae8b4c565de8
igor <igor.gritsanchouk@gmail.com>
2024-12-02 08:33:31 +0000

    added new entities and few intellij files

---
+++ webstn/src/main/java/com/mm/webstn/bootstrap/CleanDatabase.java
@@ -0,0 +1,25 @@
+package com.mm.webstn.bootstrap;
+
+//import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class CleanDatabase{ //} implements CommandLineRunner{
+    //implements ApplicationRunner {
+//    @Autowired
+//    private Flyway flyway;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("... About to clean DB....");
+//        flyway.clean();
+//        //flyway.migrate();
+//        System.out.println("... Done cleaning DB ....");
+//
+//
+//    }
+
+}
--- webstn/src/test/java/com/mm/webstn/WebstnApplicationTests.java
+++ webstn/src/test/java/com/mm/webstn/WebstnApplicationTests.java
@@ -3,10 +3,10 @@ package com.mm.webstn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WebstnApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
